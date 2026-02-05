5f8cb2c9865ef3fbb436e126f1511cafc6775bd0
João <joaowebpoa@gmail.com>
2026-02-05 14:38:09 -0300

    fix db ssl

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,6 +10,8 @@ type DatabaseConfig = Options & {
 
 type Environment = 'development' | 'test' | 'production';
 
+const enableSsl = (process.env.DB_SSL ?? '').toLowerCase() === 'true';
+
 const baseConfig: DatabaseConfig = {
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
@@ -17,7 +19,17 @@ const baseConfig: DatabaseConfig = {
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT || 5432),
   dialect: 'postgres',
-  logging: false
+  logging: false,
+  ...(enableSsl
+    ? {
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      }
+    : {})
 };
 
 const databaseConfig: Record<Environment, DatabaseConfig> = {
